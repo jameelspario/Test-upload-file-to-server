1810fc3ae260723f3785b06f64c97051b0189646
jameelspario <jameelspario@gmail.com>
2021-06-06 20:11:11 +0530

    Initial commit

---
+++ app/src/main/java/com/spario/testuploadfiletoserver/Utils.java
@@ -0,0 +1,10 @@
+package com.spario.testuploadfiletoserver;
+
+public class Utils {
+
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://192.168.43.52/fileupload/fileUpload.php";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "AndroidFileUpload";
+}
